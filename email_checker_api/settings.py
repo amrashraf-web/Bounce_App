14f3884547eb77f96152522cf91f60f814498345
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-07-12 17:30:01 +0300

    Update settings.py

--- email_checker_api/settings.py
+++ email_checker_api/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'email_checker_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'primarydb',
+        'USER': 'root',
+        'PASSWORD': 'EqjAQJFwKq3x18fx',
+        'HOST':'mysql://root:EqjAQJFwKq3x18fx@provisioning:3306/primarydb',
+        'PORT':'3306',
     }
 }
 
