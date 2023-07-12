af1950714026e0b0db1d1b8b0993a5fabb8e66a8
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-07-12 18:11:49 +0300

    Update settings.py

--- email_checker_api/settings.py
+++ email_checker_api/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+ import pymysql
+ pymysql.version_info = (1, 4, 6, 'final', 0)
+ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
